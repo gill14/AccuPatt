fix blank cards in plots and average edges

accupatt/models/passDataCard.py
@@ -25,7 +25,6 @@ class PassDataCard(PassDataBase):
                 and card.include_in_composite
                 and card.location is not None
                 and card.location_units is not None
-                and any(s["is_include"] for s in card.stains)
             ],
             key=lambda x: x.location,
         )
@@ -134,15 +133,21 @@ class PassDataCard(PassDataBase):
             # Interpolate so that fill-between looks good
             locs_i = np.linspace(d["loc"].iloc[0], d["loc"].iloc[-1], num=1000)
             cov_i = np.interp(locs_i, d["loc"], d["cov"])
+            # Handle shading for blank active cards
+            d = d.set_index("loc")
+            d.sort_values(by="loc", axis=0, inplace=True)
+            d["dv01"].interpolate(method="slinear", fill_value="extrapolate", inplace=True)
+            d["dv05"].interpolate(method="slinear", fill_value="extrapolate", inplace=True)
+            d.reset_index(inplace=True)
             # Colorize
             if colorize:
                 # Get a np array of dsc's calculated for each interpolated loc
                 kind = (
                     "linear" if cfg.get_card_plot_colorize_interpolate() else "nearest"
                 )
-                interpolator = interpolate.interp1d(d["loc"], d["dv01"], kind=kind)
+                interpolator = interpolate.interp1d(d["loc"], d["dv01"], kind=kind, fill_value="extrapolate")
                 dv01_i = interpolator(locs_i)
-                interpolator = interpolate.interp1d(d["loc"], d["dv05"], kind=kind)
+                interpolator = interpolate.interp1d(d["loc"], d["dv05"], kind=kind, fill_value="extrapolate")
                 dv05_i = interpolator(locs_i)
                 dsc_i = np.array(
                     [

accupatt/models/seriesDataCard.py
@@ -33,8 +33,13 @@ class SeriesDataCard(SeriesDataBase):
             if dd.empty:
                 dd = d
             else:
+                _d = d.set_index("loc")
+                _d.sort_values(by="loc", axis=0, inplace=True)
+                _d["dv01"].interpolate(method="slinear", fill_value="extrapolate", inplace=True)
+                _d["dv05"].interpolate(method="slinear", fill_value="extrapolate", inplace=True)
+                
                 dd = dd.merge(
-                    d.set_index("loc"),
+                    _d,
                     on="loc",
                     how="outer",
                     suffixes=[f"_{lastPassName}", f"_{p.name}"],
@@ -45,7 +50,9 @@ class SeriesDataCard(SeriesDataBase):
         dd.set_index("loc", inplace=True)
         dd.sort_values(by="loc", axis=0, inplace=True)
         dd.interpolate(method="slinear", limit_area="inside", inplace=True)
-        dd.fillna(0, inplace=True)
+        for col in dd.columns:
+            if "cov" in col:
+                dd[col].fillna(0, inplace=True)
         dd["cov_avg"] = dd.loc[:, dd.columns.str.contains("cov")].mean(axis="columns")
         dd["dv01_avg"] = dd.loc[:, dd.columns.str.contains("dv01")].mean(axis="columns")
         dd["dv05_avg"] = dd.loc[:, dd.columns.str.contains("dv05")].mean(axis="columns")

accupatt/models/sprayCard.py
@@ -218,9 +218,9 @@ class SprayCardStats:
     def set_volumetric_stats(self, drop_dia_um=None, drop_vol_um3=None):
         # Protect agains empty array
         if not any([s["is_include"] for s in self.sprayCard.stains]):
-            self.dv01 = 0
-            self.dv05 = 0
-            self.dv09 = 0
+            self.dv01 = np.nan
+            self.dv05 = np.nan
+            self.dv09 = np.nan
             return
         # dd and dv lists normally none, but will have values already for composite card calcs
         if drop_dia_um is None or drop_vol_um3 is None: